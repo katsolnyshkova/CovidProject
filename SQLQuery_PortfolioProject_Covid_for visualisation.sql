a1b27cfe0b7809daa916b277a3aa2917fe27b7ea
Ekaterina Solnyshkova <152078595+katsolnyshkova@users.noreply.github.com>
2023-11-27 23:16:24 +0300

    Add files via upload

---
+++ SQLQuery_PortfolioProject_Covid_for visualisation.sql
@@ -0,0 +1,69 @@
+
+Queries used for Tableau Project
+
+
+-- 1. 
+
+SELECT
+  SUM(d.new_cases) AS total_cases
+ ,SUM(cast(d.new_deaths AS INT)) AS total_deaths
+ ,SUM(cast(d.new_deaths AS int))/SUM(New_Cases)*100 AS DeathPercentage
+
+FROM PortfolioProject.dbo.CovidDeaths AS d
+
+WHERE d.continent IS NOT NULL
+
+ORDER BY 1,2
+
+
+-- 2. 
+
+
+SELECT
+ d.location
+ ,SUM(cast(new_deaths AS INT)) AS TotalDeathCount
+
+FROM PortfolioProject.dbo.CovidDeaths AS d
+
+WHERE 
+d.continent IS NULL
+AND d.location NOT IN ('World', 'European Union', 'International')
+
+GROUP BY d.location
+ORDER BY TotalDeathCount DESC
+
+
+-- 3.
+
+SELECT
+ d.location
+,d.population
+,MAX(d.total_cases) AS HighestInfectionCount
+,MAX((d.total_cases/d.population)*100) AS PersentPopulationInfected
+
+FROM PortfolioProject.dbo.CovidDeaths AS d
+
+GROUP BY
+ d.location
+,d.population
+
+ORDER BY PersentPopulationInfected DESC
+
+
+
+-- 4.
+SELECT
+ d.location
+,d.population
+,date
+,MAX(d.total_cases) AS HighestInfectionCount
+,MAX((d.total_cases/d.population)*100) AS PersentPopulationInfected
+
+FROM PortfolioProject.dbo.CovidDeaths AS d
+
+GROUP BY
+ d.location
+,d.population
+,d.date
+
+ORDER BY PersentPopulationInfected DESC
